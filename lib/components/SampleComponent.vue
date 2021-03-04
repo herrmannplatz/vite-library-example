4a5766b724232a84c1722a47244def86945be43e
herrmannplatz <rene@herrmannplatz.de>
2021-03-04 22:29:02 +0100

    first commit

---
+++ lib/components/SampleComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>{{ whoop }}</h1>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+export default defineComponent({
+  name: 'SampleComponent',
+  setup: () => {
+    const whoop = ref('🎉 whoop 🎉')
+    return { whoop }
+  }
+})
+</script>
